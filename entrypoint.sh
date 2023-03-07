05e650f3bb3b7f0e4af6fa8bd3208c5473af7afd
Patka Zsolt-Andras <andras.patka@gmail.com>
2023-03-07 18:19:40 +0200

    fix: Use different mysql connector, copy requirements.txt to the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 # add additional dependencies required for your solution here.
 # for example:
 # pip3 install mysql-client
-pip3 install -r requirements.txt
+pip3 install -r /scripts/requirements.txt
 sleep infinity
